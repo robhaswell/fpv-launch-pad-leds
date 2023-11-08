b6d1fb6b9e994a9681827a208cf2a929f191e52b
Rob Haswell <me@robhaswell.co.uk>
2023-11-08 10:17:58 +0000

    Terrible but working.

--- src/main.cpp
+++ src/main.cpp
@@ -17,48 +17,56 @@ uint8_t val = 100;
 // The hue increment for each LED
 uint8_t hueStep = 255 / NUM_LEDS;
 
-#define PACKET_LENGTH    12
+#define PACKET_LENGTH 12
 int cmdString[PACKET_LENGTH];
 bool recording = false;
 
-void getRecordingState(int *cmdString) {
-    int index = 0;
+void getRecordingState(int *cmdString)
+{
+  int index = 0;
 
-    if (!Serial2.available()) {
-      return;
-    }
-    Serial.println("Serial2 available");
+  if (!Serial2.available())
+  {
+    return;
+  }
+  Serial.println("Serial2 available");
 
-    // Look for starting letter 36
-    int letter = (int)Serial2.read();
-    if (letter != 36) {
-      return;
-    }
-    Serial.println("Found starting letter");
-    cmdString[index] = letter;
-    index++;
+  // Look for starting letter 36
+  int letter = (int)Serial2.read();
+  if (letter != 36)
+  {
+    return;
+  }
+  Serial.println("Found starting letter");
+  cmdString[index] = letter;
+  index++;
 
-    while (index < PACKET_LENGTH) {
-      if (Serial2.available()) {
-        int letter = (int)Serial2.read();
-        Serial.println(letter);
-        cmdString[index] = letter;
-        index++;
+  while (index < PACKET_LENGTH)
+  {
+    if (Serial2.available())
+    {
+      int letter = (int)Serial2.read();
+      Serial.println(letter);
+      cmdString[index] = letter;
+      index++;
     }
   }
 
   Serial.println("Got command string: ");
-  for (int i = 0; i < PACKET_LENGTH; i++) {
+  for (int i = 0; i < PACKET_LENGTH; i++)
+  {
     Serial.println(cmdString[i]);
   }
 
-  if (cmdString[PACKET_LENGTH - 1] == 74) {
+  if (cmdString[PACKET_LENGTH - 1] == 74)
+  {
     recording = true;
-  } else {
+  }
+  else
+  {
     recording = false;
   }
 }
-
 
 void setup()
 {
@@ -81,27 +89,27 @@ void loop()
   getRecordingState(cmdString);
 
   if (recording) {
-    leds[0] = CRGB::Red;
+    for (int i = 0; i < NUM_LEDS; i++)
+      leds[i].setHSV(0, sat, hue);
+      
   } else {
-    leds[0] = CRGB::Green;
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+      uint8_t ledHue = hue + abs8((15 - i) * hueStep);
+      // uint8_t ba = hue + (i * hueStep);
+      leds[i].setHSV(ledHue, sat, val);
+    }
+    FastLED.show();
+
+    // Increment the hue to give the rotating effect
   }
+  hue += SPEED;
   FastLED.show();
 
   delay(DELAY);
 }
-  // // put your main code here, to run repeatedly:
-  // for (int i = 0; i < NUM_LEDS; i++)
-  // {
-  //   uint8_t ledHue = hue + abs8((15 - i) * hueStep);
-  //   // uint8_t ba = hue + (i * hueStep);
-  //   leds[i].setHSV(ledHue, sat, val);
-  // }
-  // FastLED.show();
-  // delay(DELAY);
+// // put your main code here, to run repeatedly:
 
-  // // Increment the hue to give the rotating effect
-  // hue += SPEED;
-
-  // // if (msp.recv(&packet.recvMessageID, packet.payload, packet.maxSize, &packet.recvSize)) {
-  // //   FastLED.show();
-  // // }
+// // if (msp.recv(&packet.recvMessageID, packet.payload, packet.maxSize, &packet.recvSize)) {
+// //   FastLED.show();
+// // }
