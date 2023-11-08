1db1770b38606fadc6f02625c57b7752cf95bac2
Rob Haswell <me@robhaswell.co.uk>
2023-11-08 11:45:44 +0000

    Rainbow chasing pattern.

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,19 @@
 #define LED_PIN 0
 #define DELAY 16
 
-#define SPEED 5
+#define SPEED 1
 
 CRGB leds[NUM_LEDS];
 
 // The starting hue of the LED string
 uint8_t hue = 0;
 uint8_t sat = 255;
-uint8_t val = 255;
+uint8_t val = 0;
+uint8_t speed = SPEED;
 
 // The hue increment for each LED
-uint8_t hueStep = 255 / NUM_LEDS;
+uint8_t hueStep = 10;
+uint8_t valStep = 30;
 
 bool recording = false;
 
@@ -29,7 +31,9 @@ void setup()
   Serial.begin();
 
   // Initialise LEDs
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip);
+  FastLED.clear();
 
   Serial2.setTX(8);
   Serial2.setRX(9);
@@ -55,23 +59,27 @@ void loop()
 
   if (recording)
   {
+    speed = 6;
     for (int i = 0; i < NUM_LEDS; i++)
       leds[i].setHSV(0, sat, sin8(hue * 1.5));
   }
   else
   {
+    speed = SPEED;
     for (int i = 0; i < NUM_LEDS / 2; i++)
     {
       uint8_t ledHue = hue + i * hueStep;
-      leds[i].setHSV(ledHue, sat, val);
+      uint8_t ledVal = val + i * valStep;
+      leds[i].setHSV(ledHue, sat, sin8(ledVal));
     }
 
     // Mirror the LEDs to the other side
     for (int i = 0; i < NUM_LEDS / 2; i++)
-      leds[NUM_LEDS - i - 1].setRGB(leds[i].r, leds[i].g, leds[i].b);
+      leds[NUM_LEDS - i - 1] = leds[i];
   }
   FastLED.show();
-  hue += SPEED;
+  hue += speed;
+  val += (speed * 5);
 
   delay(DELAY);
 }
