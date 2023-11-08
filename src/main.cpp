7774bcbebd23215677dac988d73a2f4ced029228
Rob Haswell <me@robhaswell.co.uk>
2023-11-08 10:38:46 +0000

    Use the MSP library from ExpressLRS.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <msp.h>
+#include <msptypes.h>
 
 #define NUM_LEDS 32
 #define LED_PIN 0
@@ -17,56 +19,9 @@ uint8_t val = 100;
 // The hue increment for each LED
 uint8_t hueStep = 255 / NUM_LEDS;
 
-#define PACKET_LENGTH 12
-int cmdString[PACKET_LENGTH];
 bool recording = false;
 
-void getRecordingState(int *cmdString)
-{
-  int index = 0;
-
-  if (!Serial2.available())
-  {
-    return;
-  }
-  Serial.println("Serial2 available");
-
-  // Look for starting letter 36
-  int letter = (int)Serial2.read();
-  if (letter != 36)
-  {
-    return;
-  }
-  Serial.println("Found starting letter");
-  cmdString[index] = letter;
-  index++;
-
-  while (index < PACKET_LENGTH)
-  {
-    if (Serial2.available())
-    {
-      int letter = (int)Serial2.read();
-      Serial.println(letter);
-      cmdString[index] = letter;
-      index++;
-    }
-  }
-
-  Serial.println("Got command string: ");
-  for (int i = 0; i < PACKET_LENGTH; i++)
-  {
-    Serial.println(cmdString[i]);
-  }
-
-  if (cmdString[PACKET_LENGTH - 1] == 74)
-  {
-    recording = true;
-  }
-  else
-  {
-    recording = false;
-  }
-}
+MSP msp;
 
 void setup()
 {
@@ -86,13 +41,25 @@ void setup()
 
 void loop()
 {
-  getRecordingState(cmdString);
+  if (Serial2.available())
+  {
+    uint8_t c = Serial2.read();
+    if (msp.processReceivedByte(c))
+    {
+      mspPacket_t *packet = msp.getReceivedPacket();
+      if (packet->function == MSP_ELRS_BACKPACK_SET_RECORDING_STATE)
+        recording = packet->readByte();
+      msp.markPacketReceived();
+    }
+  }
 
-  if (recording) {
+  if (recording)
+  {
     for (int i = 0; i < NUM_LEDS; i++)
       leds[i].setHSV(0, sat, hue);
-      
-  } else {
+  }
+  else
+  {
     for (int i = 0; i < NUM_LEDS; i++)
     {
       uint8_t ledHue = hue + abs8((15 - i) * hueStep);
@@ -108,8 +75,3 @@ void loop()
 
   delay(DELAY);
 }
-// // put your main code here, to run repeatedly:
-
-// // if (msp.recv(&packet.recvMessageID, packet.payload, packet.maxSize, &packet.recvSize)) {
-// //   FastLED.show();
-// // }
